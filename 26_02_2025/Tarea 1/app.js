570046687751c8d20c7f1c918f79bb916a56c292
AlexaOrtega0412 <alexavob@gmail.com>
2025-03-17 23:42:27 -0600

    Codigos_Clase 26-02-2025

---
+++ 26_02_2025/Tarea 1/app.js
@@ -0,0 +1,71 @@
+let getAPI = document.getElementById("getAPIBtn");
+getAPI.addEventListener('click', function(){
+
+    let listado = document.getElementById("pokeGrid");
+    listado.innerHTML = "";
+    let cardContent = "";
+
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=32&offset=0')
+        .then(response =>{return response.json()})
+        .then(data =>{
+            console.log(data)
+
+            //generamos carta por carta
+            data.results.forEach(item => {
+                //revisamos los nombres obtenidos
+                console.log(item.name);
+
+                let imgContainerID = `imgContainer${item.name}`;
+                cardContent += `
+                <div class="pokeCard">
+                    <h4>${item.name}</h4>
+                    <div><a href="${item.url}">Informaciòn</a></div>
+                    <div id="${imgContainerID}" class="contenedorInfoImagen"></div>
+                </div>
+                `;
+                
+                getDataPoke(item.url,imgContainerID);
+            })
+            //despues de generar todas las tarjetas, dibujamos
+            listado.innerHTML = cardContent;
+        })
+        .catch(error =>{console.error(error)});
+})
+
+function getDataPoke(url,imgContainerID) {    
+    //identificamos contenedor de imagen (no funciona)
+    let img = document.getElementById(imgContainerID);
+    fetch(url)
+        .then(response =>{return response.json()})
+        .then(data =>{
+            //nombre y liga de imagen del pokemon
+            console.log(`Pokemon: ${imgContainerID}`);
+            console.log(data.sprites.front_shiny);
+
+            //identificamos contenedor de imagen (si funciona)
+            let img = document.getElementById(imgContainerID);
+            img.innerHTML = `
+            <img src="${data.sprites.front_shiny}" onclick="showData(${data.id})" width="100"/>
+            <div class="infoPokemon" id="pokemon${data.id}">
+            <p>Altura: ${data.height}</p>
+            <p>Especie: ${data.species.name}</p>
+            <p>Habilidad: ${data.abilities[0]. ability.name}</p>
+            </div>
+            `;
+        })
+        .catch(error =>{console.error(error)});
+}
+
+function showData(id) {    
+    let divID = "pokemon"+id;
+    console.log("el id es: "+divID)
+    let descript = document.getElementById(divID);
+    //descript.style.display = "block";
+
+    if (descript.style.display === "block") {
+        descript.style.display = "none";
+    }else{
+        descript.style.display = "block"
+    }
+}
+
